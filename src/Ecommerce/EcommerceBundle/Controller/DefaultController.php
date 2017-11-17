eb8d12189ba4a40834ffecbad3b0b4512da17537
KeskesBilel <bilel-keskes-2008>
2017-11-18 00:33:07 +0100

    ajout a la bese de donnee par un form

--- src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
+++ src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace Ecommerce\EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Ecommerce\EcommerceBundle\Entity\article;
+use Ecommerce\EcommerceBundle\Form\articleType;
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -17,5 +20,24 @@ class DefaultController extends Controller
     public function pageproduitAction()
     {
         return $this->render('EcommerceBundle:Default:produit.html.twig');
+    }
+    public function pageajouteAction()
+    {
+      $em = $this->getDoctrine()->getManager();
+      $a = new article();
+      $form = $this->createForm(new articleType(),$a);
+      $request = $this->getRequest();
+      if ($request->isMethod('POST')) {
+       $form->submit($request);
+
+        //$form->bind($request);
+      $em->persist($a);
+      $em->flush();
+
+       return $this->redirect($this->generateUrl("ecommerce_ajoutepage"));
+      }
+
+       return $this->render('EcommerceBundle:Default:ajoute.html.twig', array('form' => $form->createView() ));
+
     }
 }
