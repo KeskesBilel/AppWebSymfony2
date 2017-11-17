7979b2ffab9f9af20e604e89a542bd967d949f6c
KeskesBilel <bilel-keskes-2008>
2017-11-17 23:43:33 +0100

    ajoute de page produit,contact
    ajouter des pages produit et contact et config les controller et routing

--- src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
+++ src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
@@ -10,4 +10,12 @@ class DefaultController extends Controller
     {
         return $this->render('EcommerceBundle:Default:index.html.twig');
     }
+    public function pagecontactAction()
+    {
+        return $this->render('EcommerceBundle:Default:contact.html.twig');
+    }
+    public function pageproduitAction()
+    {
+        return $this->render('EcommerceBundle:Default:produit.html.twig');
+    }
 }
