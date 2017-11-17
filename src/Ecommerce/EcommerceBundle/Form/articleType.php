f35378224e1e82cf7640e4c736f58fc002fb6b41
KeskesBilel <bilel-keskes-2008>
2017-11-18 00:09:15 +0100

    ecrire un Form
    ecrire un FORM avec les  le commende
    doctrine:generate:form NomBundle:NomTAB
    apres le commend un doussier de name fom ajouter sur scr avec un fichier

---
+++ src/Ecommerce/EcommerceBundle/Form/articleType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class articleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prix')
+            ->add('tva')
+            ->add('cat')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ecommerce\EcommerceBundle\Entity\article'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ecommerce_ecommercebundle_article';
+    }
+}
